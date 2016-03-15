2c5ea32e979f77f99fdcee4fdcd279e5272566fa
Piotr Pałek <piotrpalek@gmail.com>
2016-03-15 11:55:57 +0100

    show point data in alert

--- demo/example1/application.js
+++ demo/example1/application.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    plotClick({ points }) {
+      const xys = points.map(point => `x: ${point.x} | y: ${point.y}\n`);
+      alert(xys);
+    }
+  },
+
   plotData: [{
     name: 'test plot',
     x: [1, 2, 3, 4],
